857dfcaaec816748d2fa6ac851c4e53ae320549a
BEN MAGARE <magaben33@gmail.com>
2023-07-17 13:04:09 +0300

    testing

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -52,6 +52,7 @@ class Payment extends Model
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $curl_response = curl_exec($curl);
         $json = json_decode($curl_response, true);
+        return $json;
         $access_token = $json['access_token'];
         $passkey = $account->mpesa_pass_key;
         $timestamp = '30' . date("ymdhis");
