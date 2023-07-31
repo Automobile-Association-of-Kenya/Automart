e5c8319245cbe19aee7679b51a51e14106e8af42
BEN MAGARE <magaben33@gmail.com>
2023-07-31 16:34:26 +0300

    dkhsd

--- database/migrations/2023_07_17_122627_add_type_column_to_socials_table.php
+++ database/migrations/2023_07_17_122627_add_type_column_to_socials_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('socials', function (Blueprint $table) {
-            $table->string('type',30)->after('id')->nullable();
+            // $table->string('type',30)->after('id')->nullable();
         });
     }
 
