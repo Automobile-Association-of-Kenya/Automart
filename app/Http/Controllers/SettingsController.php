59edb71fa974561739e8c9a425b7959167524a90
BEN MAGARE <magaben33@gmail.com>
2023-08-07 19:53:18 +0300

    More changes

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -194,7 +194,7 @@ class SettingsController extends Controller
     }
 
     public function bulkMail(Request $request) {
-        
+
         $files = collect();
         if ($request->hasFile('attachments')) {
             foreach ($request->file('attachments') as $file) {
@@ -207,7 +207,7 @@ class SettingsController extends Controller
             if (count($request->users) > 0) {
                 foreach ($request->users as $value) {
                     $user = $this->user->find($value);
-                    Mail::to("magaben33@gmail.com",$user->name)->send(new Bulk($request->subject, $request->message,$files));
+                    Mail::to($user->email,$user->name)->send(new Bulk($request->subject, $request->message,$files));
                 }
             }else {
                 return json_encode(['status'=>"error", 'message'=>"No users selected to email"]);
--- resources/views/emails/Bulk.blade.php
+++ resources/views/emails/Bulk.blade.php
@@ -1,9 +1,9 @@
 <x-mail::message>
+    <img src="{{ asset('images/automart.jpg') }}" alt="">
 <h5 style="color: #006544; text-align: center;">{{ $subject }}</h5>
-
 {!! $message !!}
 @foreach ($attachments as $item)
-    <img src="{{ public_path("attachments/$item") }}">
+    <img src="{{ asset("attachments/$item") }}">
 @endforeach
 <br>
 Thanks,<br>
