8bbd4d8f1b91fdab251963bb49a8f823c53990ae
BEN MAGARE <magaben33@gmail.com>
2023-08-17 10:22:16 +0300

    More changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,3 @@
-
 <!-- Footer start -->
 <footer class="footer">
     <div class="clearfix"></div>
@@ -11,7 +10,11 @@
                         <div class="s-border"></div>
                         <div class="m-border"></div>
                         <div class="text">
-                            <P class="mb-0">With AA Automart, car sellers have the opportunity to showcase their vehicles to a wide audience of potential buyers, maximizing their chances of making a successful sale. On the other hand, car buyers can browse through a diverse inventory of cars available for purchase, making the platform a one-stop destination for all their automotive needs.</P>
+                            <P class="mb-0">With AA Automart, car sellers have the opportunity to showcase their
+                                vehicles to a wide audience of potential buyers, maximizing their chances of making a
+                                successful sale. On the other hand, car buyers can browse through a diverse inventory of
+                                cars available for purchase, making the platform a one-stop destination for all their
+                                automotive needs.</P>
                         </div>
                     </div>
                 </div>
@@ -25,11 +28,13 @@
                         <div class="m-border"></div>
                         <ul class="contact-info">
                             <li>
-                                <i class="flaticon-pin"></i>8th Floor, Renaissance Corporate Park, Upper Hill, Nairobi, Kenya
+                                <i class="flaticon-pin"></i>8th Floor, Renaissance Corporate Park, Upper Hill, Nairobi,
+                                Kenya
                             </li>
 
                             <li>
-                                <i class="flaticon-mail"></i><a href="mailto:customercare@aakenya.co.ke">customercare@aakenya.co.ke</a>
+                                <i class="flaticon-mail"></i><a
+                                    href="mailto:customercare@aakenya.co.ke">customercare@aakenya.co.ke</a>
                             </li>
                             <li>
                                 <i class="flaticon-phone"></i><a href="tel:+254709933999">+254709933999 </a>
@@ -66,7 +71,8 @@
                             </li>
 
                             <li>
-                                <a href="{{ route('vehicles.discounts') }}"><i class="fa fa-angle-right"></i>Discounts</a>
+                                <a href="{{ route('vehicles.discounts') }}"><i
+                                        class="fa fa-angle-right"></i>Discounts</a>
                             </li>
 
                             <li>
@@ -90,7 +96,6 @@
         </div>
     </div>
 
-
     <div class="sub-footer">
         <div class="container">
             <div class="row">
@@ -100,25 +105,33 @@
                 <div class="col-lg-6 col-md-6">
                     <div class="social-media clearfix">
                         <div class="social-list">
-                            <div class="icon facebook">
-                                <div class="tooltip">Facebook</div>
-                                <span><i class="fa fa-facebook"></i></span>
-                            </div>
+                            <a href=" https://www.facebook.com/AAKenya" target="_blank">
+                                <div class="icon facebook">
+                                    <div class="tooltip">Facebook</div>
+                                    <span><i class="fa fa-facebook"></i></span>
+                                </div>
+                            </a>
 
-                            <div class="icon twitter">
-                                <div class="tooltip">Twitter</div>
-                                <span><i class="fa fa-twitter"></i></span>
-                            </div>
+                            <a href="https://twitter.com/AAKenya" target="_blank">
+                                <div class="icon twitter">
+                                    <div class="tooltip">Twitter</div>
+                                    <span><i class="fa fa-twitter"></i></span>
+                                </div>
+                            </a>
 
-                            <div class="icon instagram">
-                                <div class="tooltip">Instagram</div>
-                                <span><i class="fa fa-instagram"></i></span>
-                            </div>
+                            <a href=" https://www.instagram.com/aa_kenya_/" target="_blank">
+                                <div class="icon instagram">
+                                    <div class="tooltip">Instagram</div>
+                                    <span><i class="fa fa-instagram"></i></span>
+                                </div>
+                            </a>
 
-                            <div class="icon youtube mr-0">
-                                <div class="tooltip">Youtube</div>
-                                <span><i class="fa fa-youtube"></i></span>
-                            </div>
+                            <a href="https://www.youtube.com/@AA_Kenya" target="_blank">
+                                <div class="icon youtube mr-0">
+                                    <div class="tooltip">Youtube</div>
+                                    <span><i class="fa fa-youtube"></i></span>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -129,7 +142,7 @@
 
 <div id="full-page-search">
     <button type="button" class="close">×</button>
-    <form action="https://storage.googleapis.com/theme-vessel-items/checking-sites/autocar-html/HTML/main/index.html#" class="full-page-search-inner">
+    <form class="full-page-search-inner">
         <input type="search" value="" placeholder="type keyword(s) here" />
         <button type="submit" class="btn btn-sm button-theme">Search</button>
     </form>
